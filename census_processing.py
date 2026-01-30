f55d9f497594c0561fec3e5a14850c4c739295ad
Antonio Nico Santos <antonio.nico.santos@gmail.com>
2026-01-30 19:39:02 -0300

    Created: gym geocoding, code to process Census data

---
+++ census_processing.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 18:34:21 2026
+
+@author: Nico Antonio (Antonio Augusto Santos)
+"""
+
+import geopandas as gpd
+import pandas as pd
+import os
+from config import path_gpkg 
+
+# 1. Loading Files that where downloaded from ISTAT
+# The names of the files correspond to the original from the site
+# https://www.istat.it/notizia/basi-territoriali-e-variabili-censuarie/
+shp_path = os.path.join(path_gpkg, "R01_21_WGS84.shp")
+xlsx_path = os.path.join(path_gpkg, "R01_indicatori_2021_sezioni.xlsx")
+
+gdf_sezioni = gpd.read_file(shp_path)
+df_dati = pd.read_excel(xlsx_path)
+
+# 2. Conversion of the field of the Join to string
+gdf_sezioni['SEZ21_ID'] = gdf_sezioni['SEZ21_ID'].astype(str)
+df_dati['SEZ21_ID'] = df_dati['SEZ21_ID'].astype(str)
+
+# 3. Join of the data
+# The field of the join is the 'SEZ21_ID', every census district got a different ID
+# It is composed of the code of the district + PROCOM (code of the commune)
+gdf_completo = gdf_sezioni.merge(df_dati, on='SEZ21_ID')
+
+# 4. Selecting the data of the communes of interest
+# The PROCOM code values for Torino (1272) and Pinerolo (1191)
+# Checking for string or integer
+codici_comuni = ['1191', '1272', 1191, 1272]
+gdf_final = gdf_completo[gdf_completo['PROCOM'].isin(codici_comuni)].copy()
+
+# 5. Saving the data to a GeoPackage
+output_path = os.path.join(path_gpkg, "censimento.gpkg")
+gdf_final.to_file(output_path, layer='sezioni_censimento', driver="GPKG")
+
+print(f" Sucessly saved in {output_path}")
---
+++ pinerolo_geocoding_palestra.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 30
+
+@author: Nico Antonio (Antonio Augusto Santos)
+"""
+
+import time
+import os
+import googlemaps
+import geopandas as gpd
+from config import API_KEY, path_gpkg
+from shapely.geometry import Point
+
+# Initial setup
+API_KEY = API_KEY
+gmaps = googlemaps.Client(key=API_KEY)
+data_list = []
+next_page_token = None
+
+# Parameters for query
+query = "Palestra, Pinerolo"
+
+# Search 
+while True:
+    # At first, there is no token.
+    if next_page_token:
+        # Wait 3 seconds for a new requesto
+        time.sleep(3)
+        places_result = gmaps.places(query=query, page_token=next_page_token)
+    else:
+        places_result = gmaps.places(query=query)
+
+    for place in places_result['results']:
+        place_id = place.get('place_id')
+          
+        lat = place['geometry']['location']['lat']
+        lng = place['geometry']['location']['lng']
+        
+        data_list.append({
+            'name': place.get('name'),
+            'address': place.get('formatted_address'),
+            'rating_note': place.get('rating', 0),
+            'num_reviews': place.get('user_ratings_total', 0),
+            'city': "Pinerolo",
+            'geometry': Point(lng, lat),
+            'class' : ", ".join(place.get('types', []))
+        })
+    next_page_token = places_result.get('next_page_token')
+    
+    #If there is no other token, the loop is closed
+    if not next_page_token:
+        break
+        
+#defining gdf    
+gdf = gpd.GeoDataFrame(data_list, crs="EPSG:4326")    
+# To add the data to a GeoPackage:
+gpkg_file = "pinerolo_locations.gpkg"
+gpkg = os.path.join(path_gpkg,gpkg_file)
+gdf.to_file(
+    gpkg, 
+    layer='palestra', 
+    driver="GPKG", 
+    mode='w'
+    )
+
+
+
